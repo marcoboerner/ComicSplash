de0678a0078f1bb8402b96616f2eb97897f69f18
Marco Boerner <mail@marcoboerner.com>
2021-07-16 18:11:11 +0200

    Initial Commit

---
+++ ComicSplash/ComicSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicSplashApp.swift
+//  ComicSplash
+//
+//  Created by Marco Boerner on 16.07.21.
+//
+
+import SwiftUI
+
+@main
+struct ComicSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
